eeb7558cbe3c67328e23a12d0a733e1ad50de5f2
Janar <jtsava@tktk.ee>
2022-09-24 12:35:51 +0300

    add week2 code

---
+++ UserAgeValidation/Program.cs
@@ -0,0 +1,19 @@
+﻿//Rakendus küsib kasutaja vanust
+//Kui on noorem kui 13. aastane kuvab:
+//"You are too young to use Instagram"
+//Kui vanem kuvab:
+//"Welcome to Instagram"
+
+
+Console.WriteLine("Enter your age:");
+
+int userAge = Int32.Parse(Console.ReadLine());
+
+if(userAge >= 13)
+{
+    Console.WriteLine("Welcome to Instagram!");
+}
+else
+{
+    Console.WriteLine("You are too young to use Instagram.");
+}
---
+++ UserGenderValidation/Program.cs
@@ -0,0 +1,28 @@
+﻿//Rakendus küsib kasutaja sugu (m/f) ja perekonnanime
+//Lähtudes kasutaja sisestusest tervitab:
+//"Welcome, Mr. [perekonnanimi]" / "Welcome, Ms. [perekonnanimi]"
+
+Console.WriteLine("Please, select your gender (m/f):");
+
+char userGender = Char.Parse(Console.ReadLine());
+//Console.ReadLine() loeb andmeid ainult string formaadis
+
+Console.WriteLine("Please, enter your last name:");
+
+string userLastName = Console.ReadLine();
+
+if (userGender == 'm')
+{
+    Console.WriteLine($"Welcome, Mr. {userLastName}!");
+}
+else if(userGender == 'f')
+{
+    Console.WriteLine($"Welcome, Ms. {userLastName}!");
+}
+else
+{
+    Console.WriteLine($"Welcome, {userLastName}!");
+}
+//if mitu korda järjest kasutades kontrollib alati kõik versioonid üle
+//else if puhul kui esimene sobib siis edasi ei kontrolli
+//Väiksem koormus arvutile
